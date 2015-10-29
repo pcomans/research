579c41ae20a46ff4354aff64b25a34a4943aa2da
Eonist <unicornhive@gmail.com>
2015-10-29 19:17:10 +0100

    New files added: 1
    Added 1 file:swift/types/ns/NSBezierTest.swift

---
+++ swift/types/ns/NSBezierTest.swift
@@ -0,0 +1,6 @@
+Stroking a bezierpath:  (may be  a way to do it)
+http://stackoverflow.com/questions/20632365/draw-gradient-along-a-curved-uibezierpath
+
+**Really nice angled gradients in a circle. (a nod to the film "Her")**  
+[here](https://medium.com/swift-programming/how-to-create-an-angle-gradient-border-in-swift-f4856dde4c90#.k0xunjtt5) 
+![circle gradient example](https://dl.dropboxusercontent.com/u/2559476/Screen%20Shot%202015-10-29%20at%2018.45.34.png) 
