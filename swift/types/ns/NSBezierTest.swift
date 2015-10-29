e7449922885314c7034e7c968e2311ae6a334c67
Eonist <unicornhive@gmail.com>
2015-10-29 19:36:41 +0100

    Files modified: 1
    Modified 1 file:swift/types/ns/NSBezierTest.swift

--- swift/types/ns/NSBezierTest.swift
+++ swift/types/ns/NSBezierTest.swift
@@ -4,3 +4,6 @@ http://stackoverflow.com/questions/20632365/draw-gradient-along-a-curved-uibezie
 **Really nice angled gradients in a circle. (a nod to the film "Her")**  
 [here](https://medium.com/swift-programming/how-to-create-an-angle-gradient-border-in-swift-f4856dde4c90#.k0xunjtt5) 
 ![circle gradient example](https://dl.dropboxusercontent.com/u/2559476/Screen%20Shot%202015-10-29%20at%2018.45.34.png) 
+
+PaintCode example output: (greate for organizing styles etc)  
+[here](http://www.raywenderlich.com/36341/paintcode-tutorial-dynamic-buttons) 
